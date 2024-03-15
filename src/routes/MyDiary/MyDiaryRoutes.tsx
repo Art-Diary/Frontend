c42d35797e99e30c49db653a78400e57d046e013
cofls980 <qawse980@gmail.com>
2024-03-15 13:33:06 +0900

    [refactor] routes 경로 파일에 styled-component 및 반응형 디자인 적용

--- src/routes/MyDiary/MyDiaryRoutes.tsx
+++ src/routes/MyDiary/MyDiaryRoutes.tsx
@@ -1,35 +1,23 @@
-// import React from 'react';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import MyExhScreen from '../../screens/mydiary/MyExhsScreen';
-// import MyExhAddScreen from '~/screens/mydiary/MyExhAddScreen';
-// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MyDiaryListScreen from '~/screens/mydiary/mydiaryList/MyDiaryListScreen';
 
-// const MyDiary = createNativeStackNavigator();
+const MyDiary = createNativeStackNavigator();
 
-// // "내 기록"
-// const MyDiaryRoutes = () => {
-//   return (
-//     <MyDiary.Navigator initialRouteName="my-exhibitions">
-//       {/* 첫 화면인 전시회 목록 */}
-//       <MyDiary.Screen
-//         name="my-exhibitions"
-//         component={MyExhScreen}
-//         options={{headerShown: false}}
-//       />
-//       {/* 한 전시회에 대한 기록들 */}
-//       {/* <MyDiary.Screen
-//         name="my-diaries"
-//         component={MyDiariesRoutes}
-//         options={{headerShown: false}}
-//       /> */}
-//       {/* 전시회 추가하기 위한 검색 */}
-//       <MyDiary.Screen
-//         name="add-my-exhibition"
-//         component={MyExhAddScreen}
-//         options={{headerShown: false}}
-//       />
-//     </MyDiary.Navigator>
-//   );
-// };
+// "내 기록"
+const MyDiariesRoutes = () => {
+  return (
+    <MyDiary.Navigator initialRouteName="my-diaries-list">
+      {/* 한 전시회에 대한 기록들 */}
+      <MyDiary.Screen
+        name="my-diaries-list"
+        component={MyDiaryListScreen}
+        options={{headerShown: false}}
+      />
+      {/* 기록 추가 */}
+      {/* 기록 수정 */}
+    </MyDiary.Navigator>
+  );
+};
 
-// export default MyDiaryRoutes;
+export default MyDiariesRoutes;
