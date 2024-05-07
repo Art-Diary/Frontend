78a8919aed572241684e7c943ef9c603d751ada9
coflskim <qawse980@gmail.com>
2024-05-07 16:48:09 +0900

    [feature] 구글 로그인 구현

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import {API_URL} from '@env';
+// import {API_URL} from '@env';
 
 export const client = axios.create({
-  baseURL: API_URL,
+  baseURL: 'http://wifi ip 주소:8080',
   timeout: 2000,
 });
 
