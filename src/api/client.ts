e794d7cb39d97556fd661db260e44df4863eaa70
coflskim <50487221+cofls980@users.noreply.github.com>
2024-05-28 17:19:26 +0900

    [refactor] 충돌 커밋 메시지 삭제

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,10 +3,7 @@ import {API_URL} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const client = axios.create({
-  baseURL: 'http://172.30.1.1:8080',
-=======
   baseURL: API_URL,
->>>>>>> 9a34ffc45cebc8529ccf19ceb5dc6ceb3dfc56f1
   timeout: 2000,
 });
 
@@ -17,17 +14,13 @@ client.interceptors.request.use(async config => {
   if (config.url === '/users' && config.method === 'post') {
     return config;
   }
-  if (!config.headers) {
-    return config;
-  }
-  if (config.url === '/users' && config.method === 'post') {
-    return config;
-  }
   // 여기서 userId를 가져와야 한다고 가정합니다. 예를 들어, 현재 사용자의 userId를 가져오는 함수를 호출한다고 가정합니다.
   const userId = await getUserId();
 
-  // config.headers에 userId를 추가합니다.
-  config.headers['userId'] = userId;
+  if (userId) {
+    // config.headers에 userId를 추가합니다.
+    config.headers['userId'] = userId;
+  }
 
   return config;
 });
