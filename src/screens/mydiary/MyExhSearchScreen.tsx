d08b6ae3238ff2a9466757551cacc365a52a977b
coflskim <qawse980@gmail.com>
2024-06-07 16:12:27 +0900

    [merge] merge

--- src/screens/mydiary/MyExhSearchScreen.tsx
+++ src/screens/mydiary/MyExhSearchScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Keyboard} from 'react-native';
 import styled from 'styled-components/native';
 import BackView from '~/components/common/BackView';
@@ -6,16 +6,48 @@ import SearchExhList from './SearchExhList';
 import {showToast} from '~/components/common/modal/toastConfig';
 import SearchExhFrame from '~/components/exhSearch/SearchExhFrame';
 import {checkBlankInKeyword} from '~/utils/CheckKeyword';
+import {
+  exhibitionQueryKeys,
+  useFetchSearchExhInMyDiary,
+} from '~/api/queries/exhibition';
+import LoadingModal from '~/components/common/modal/LoadingModal';
+import {useQueryClient} from 'react-query';
 
 const MyExhSearchScreen = () => {
+  const queryClient = useQueryClient();
   const [searchKeyword, setSearchKeyword] = useState<string>('');
-  const [keyword, setKeyword] = useState<string>('');
+  const [exhibitionList, setExhibitionList] = useState([]);
+  const [openLoading, setOpenLoading] = useState(false);
+  const {
+    data: exhList,
+    isLoading,
+    isError,
+    isSuccess,
+    refetch,
+  } = useFetchSearchExhInMyDiary(searchKeyword);
+
+  useEffect(() => {
+    if (isError) {
+      showToast('에러 발생 ;(');
+    }
+    if (isLoading) {
+      setOpenLoading(true);
+    } else {
+      setOpenLoading(false);
+    }
+    if (isSuccess) {
+      setExhibitionList(exhList);
+      queryClient.removeQueries(
+        exhibitionQueryKeys.fetchSearchExhInMyDiary(searchKeyword),
+      );
+    }
+  }, [isSuccess, isError, isLoading]);
 
   const onPressSearch = () => {
     if (checkBlankInKeyword(searchKeyword)) {
       showToast('다시 검색해 주세요');
     } else {
-      setKeyword(searchKeyword);
+      refetch();
     }
     Keyboard.dismiss();
   };
@@ -29,14 +61,9 @@ const MyExhSearchScreen = () => {
         onPressSearch={onPressSearch}
         handleSearchKeyword={setSearchKeyword}>
         {/* 전시회 목록 */}
-        {keyword !== '' && (
-          <SearchExhList
-            searchKeyword={keyword}
-            changeIsPressed={onPressSearch}
-            forGathering={false}
-          />
-        )}
+        <SearchExhList exhList={exhibitionList} forGathering={false} />
       </SearchExhFrame>
+      {openLoading && <LoadingModal message={'전시회 조회 중 :)'} />}
     </Container>
   );
 };
