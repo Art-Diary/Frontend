82b7312fd3ba1993682f535c89b9fa86cac42ffc
coflskim <qawse980@gmail.com>
2024-06-11 14:23:02 +0900

    [bug] 모임과 전시 메이트 관련 화면 중 오류가 발생할 때 헤더가 없어지는 화면들 수정

--- src/screens/mate/gathering/addVisitExhibition/NewVisitDateOfExhInGatheringScreen.tsx
+++ src/screens/mate/gathering/addVisitExhibition/NewVisitDateOfExhInGatheringScreen.tsx
@@ -1,5 +1,5 @@
 import {useIsFocused} from '@react-navigation/native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components/native';
 import BackView from '~/components/common/BackView';
 import {showToast} from '~/components/common/modal/toastConfig';
@@ -13,6 +13,7 @@ const NewVisitDateOfExhInGatheringScreen = () => {
   const isFocused = useIsFocused();
   const visitedExhId = useVisitedExhIdInfo().exhId;
   const {enterGatheringInfo} = useEnterGatheringInfo();
+  const [openLoading, setOpenLoading] = useState(false);
   // 모임에서 방문한 날짜 리스트 가져오기
   const {
     data: storedDateList,
@@ -29,19 +30,24 @@ const NewVisitDateOfExhInGatheringScreen = () => {
     }
   }, [isFocused]);
 
-  if (isError) {
-    showToast('오류가 발생했습니다.');
-  }
-  if (isLoading) {
-    <LoadingModal message="방문 날짜 조회 중" />;
-  }
+  useEffect(() => {
+    if (isError) {
+      showToast('모임의 방문 날짜 조회 실패 ;(');
+    }
+    if (isLoading) {
+      setOpenLoading(true);
+    } else {
+      setOpenLoading(false);
+    }
+  }, [isError, isLoading]);
 
   return (
     <Container>
       <BackView line={false} children={null} />
-      {isSuccess && (
-        <NewVisitDateInGathering markedDates={storedDateList.dates} />
-      )}
+      <NewVisitDateInGathering
+        markedDates={storedDateList ? storedDateList.dates : []}
+      />
+      {openLoading && <LoadingModal message={'방문 날짜 조회 중 :)'} />}
     </Container>
   );
 };
