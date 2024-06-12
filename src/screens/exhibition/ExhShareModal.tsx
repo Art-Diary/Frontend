25e95478d5ed4c4c8b5285a19449127d64b3069f
hiwon2 <leehw1997@gmail.com>
2024-06-12 14:46:04 +0900

    sharetokakao 일단 보류

--- src/screens/exhibition/ExhShareModal.tsx
+++ src/screens/exhibition/ExhShareModal.tsx
@@ -19,27 +19,27 @@ const ExhShareModal: React.FC<ModalProps> = ({
   handleCloseModal,
   shareWithInsta,
 }) => {
-  const shareToKakao = async () => {
-    try {
-      const result = await sendText({
-        text: '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.',
-        link: {
-          // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-          //mobileWebUrl: 'https://developers.kakao.com',
-          //  webUrl: 'https://developers.kakao.com',
-        },
-      });
-      console.log(result);
-    } catch (error) {
-      console.error('카카오톡 공유 실패', error);
-    }
-  };
+  // const shareToKakao = async () => {
+  //   try {
+  //     const result = await sendText({
+  //       text: '기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.',
+  //       link: {
+  //         // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+  //         //mobileWebUrl: 'https://developers.kakao.com',
+  //         //  webUrl: 'https://developers.kakao.com',
+  //       },
+  //     });
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.error('카카오톡 공유 실패', error);
+  //   }
+  // };
   return (
     <ConfirmationModal handleCloseModal={handleCloseModal}>
       <ModalContainer>
         <Message>{'공유하기'}</Message>
         <ModalContentView>
-          <TouchableOpacity onPress={shareToKakao}>
+          <TouchableOpacity>
             <SnsView>
               <KakaoIconBig />
               <Sns>{'카카오톡'}</Sns>
